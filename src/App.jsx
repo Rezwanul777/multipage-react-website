b44e30d67898773ec746377f862d44232086b889
Rezwanu Haque Prince <rezwanul.haquebd777@gmail.com>
2023-02-02 20:53:14 -0800

    vite project setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import About from './About'
+import Contact from './Contact'
+import Home from './Home'
+import Services from './Services'
+
+const App = () => {
+  return (
+    <div>
+     <Home/>
+     <About/>
+     <Services/>
+     <Contact/>
+    </div>
+  )
+}
+
+export default App
