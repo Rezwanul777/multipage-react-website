421a8d428ade5fdf9d8066184647fe612f466bf2
Rezwanu Haque Prince <rezwanul.haquebd777@gmail.com>
2023-02-07 00:35:54 +0600

    Styled Componenet add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,31 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import logo from '../assets/logo.png'
 import Nav from './Nav'
+import styled from 'styled-components'
 
 const Header = () => {
   return (
     <>
-  <header>
+  <MainHeader>
   <NavLink to='/'>
-      <img src={logo} height="60px" width={60} alt="logo is not find" />
+      <img src={logo} alt="logo is not find" className='logo'/>
    </NavLink>
    <Nav/>
-  </header>
+  </MainHeader>
     </>
   )
 }
+const MainHeader=styled.header`
+padding: 0 4.8rem;
+height: 10rem;
+background-color:${({theme})=>theme.colors.bg};
+display:flex;
+justify-content: space-between;
+align-items: center;
+.logo{
+  height: 4rem;
+  max-width: 100%;
+}
 
+`
 export default Header
